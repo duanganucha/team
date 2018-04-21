3b3e616fa73ea2e2ec663f6bea0d389296908d70
AmbulanceMan <duanganucha@hotmail.com>
2018-04-22 00:48:50 +0700

    add Angfire call agm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,25 +9,48 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from '../environments/environments'
+import { DetailPage } from '../pages/detail/detail';
+
+import { MapPage } from './../pages/map/map';
+import { AgmCoreModule } from '@agm/core';
+import { CallNumber } from '@ionic-native/call-number';
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    DetailPage,
+    MapPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCF3aH2Bk1v1-AOIaeDWFXzfsNjK0dh558'
+    }),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    DetailPage,
+    MapPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
+    CallNumber,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyBaUaU7pL-yZXpUgbNnGsAqZ45FMQt3v1w",
+      authDomain: "emergencycall-902f1.firebaseapp.com",
+      databaseURL: "https://emergencycall-902f1.firebaseio.com",
+      projectId: "emergencycall-902f1",
+      storageBucket: "emergencycall-902f1.appspot.com",
+      messagingSenderId: "786404608393"
+    }
+  };
